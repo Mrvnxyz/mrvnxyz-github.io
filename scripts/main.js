e093f06f5d12d128b026f7f421d17a060b7e589c
Marvin Gempes <khristinemaelim@gmail.com>
2024-12-13 21:09:27 +0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+
+console.log("Hello World!")
+console.log("Web Dev 101")
+console.log("Ayman C.")
+
+const myVariable = false
+const myArray = [12, "Dev", 101, "Ayman", 6.10, -120]
+console.log (myArray[myArraw.length-1])
+const myVar ="racecar"
+
+const firstNum = 120
+const secondNum = 7
+const sum = firstNum + secondNum
+const diff = firstNumber - secondNum
+const result = firstNum % secondNum
+
+function myFunc(x,y,) {
+  return x + y
+}
+
+console.log(myFunc(50,100))
+function difference(x,y) {
+  return x - y
+}
